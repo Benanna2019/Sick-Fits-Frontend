c18e938b8051023cfe9d3d4231b14d90c6a1a35f
Benanna2019 <bass41992ben@gmail.com>
2021-03-17 11:11:48 -0400

    updates while work comp was being fixed

--- components/Product.js
+++ components/Product.js
@@ -5,18 +5,30 @@ import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
 
 export default function Product({ product }) {
-	return (
-		<ItemStyles>
-			<img
-				src={product?.photo?.image?.publicUrlTransformed}
-				alt={product.name}
-			/>
-			<Title>
-				<Link href={`/product/${product.id}`}>{product.name}</Link>
-			</Title>
-			<PriceTag>{formatMoney(product.price)}</PriceTag>
-			<p>{product.description}</p>
-			{/* TODO: Add buttons to edit and delete an item */}
-		</ItemStyles>
-	);
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: Add buttons to edit and delete an item */}
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: "update",
+            query: {
+              id: product.id,
+            },
+          }}
+        >
+          Edit
+        </Link>
+      </div>
+    </ItemStyles>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,28 @@ import Page from "../components/Page";
 import "../components/styles/nprogress.css";
 import { ApolloProvider } from "@apollo/client";
 import withData from "../lib/withData";
-import { Component } from "react";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps, apollo }) {
-	return (
-		<ApolloProvider client={apollo}>
-			<Page>
-				<Component {...pageProps} />
-			</Page>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
 }
 
-MyApp.getInitialProps = async function ({ component, ctx }) {
-	let pageProps = {};
-	if (Component.getInitialProps) {
-		pageProps = await Component.getInitialProps(ctx);
-	}
-	pageProps.query = ctx.query;
-	return { pageProps };
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
 };
 
 export default withData(MyApp);
